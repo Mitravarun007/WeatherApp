bae911a70f0c1f9baebfcb1383b94a2397988c68
Mitravarun Chauhan <mitravarun.chauhan@gmail.com>
2026-07-15 02:31:08 +0530

    Initial commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Mitravarun Chauhan on 15/07/26.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
